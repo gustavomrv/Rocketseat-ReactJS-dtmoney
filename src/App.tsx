3d76b16694207cd062c04728cfd014920c351a7b
gustavomrv <gustavomourago@gmail.com>
2022-10-24 09:46:27 -0300

    Projeto finalizado

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal"
+
+import { GlobalStyle } from "./styles/global";
+import { TransactionsProvider } from './hooks/useTransactions';
+
+Modal.setAppElement('#root');
+
+export function App() {
+  const [isNewTransactionModalOpen, setisNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+      setisNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+      setisNewTransactionModalOpen(false);
+  }
+
+  return (
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
+      <Dashboard />
+
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
+      <GlobalStyle />
+    </TransactionsProvider>
+  );
+}
